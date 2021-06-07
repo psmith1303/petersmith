2185fdef046bbda54d659a6114ac4f523c36a9b7
Peter Smith <mail@petersmith.org>
2021-06-07 16:36:41 +1200

    ${DESCRIPTION}

--- Commit.sh
+++ Commit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
-read -p "Commit description: " desc
-git add . && \
-git add -u && \
-git commit -m "$desc" && \
+DESCRIPTION=${@:-"A new post"}
+
+echo "The commit message is:    " \"${DESCRIPTION}\"
+echo "The working directory is :" `pwd`
+
+git add . &&  git add -u && \
+echo git commit -m '${DESCRIPTION}' && \
+git commit -m '${DESCRIPTION}' && \
 git push origin master
