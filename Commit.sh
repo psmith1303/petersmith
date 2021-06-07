caa8406eb108917a4edf09df51d2f5a5345fcd54
Peter Smith <mail@petersmith.org>
2021-06-07 16:38:03 +1200

    \noew then\

--- Commit.sh
+++ Commit.sh
@@ -6,6 +6,6 @@ echo "The commit message is:    " \"${DESCRIPTION}\"
 echo "The working directory is :" `pwd`
 
 git add . &&  git add -u && \
-echo git commit -m '${DESCRIPTION}' && \
-git commit -m '${DESCRIPTION}' && \
+echo git commit -m \\"${DESCRIPTION}\\" && \
+git commit -m \\"${DESCRIPTION}\\" && \
 git push origin master
