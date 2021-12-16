5e9e52ea624c616483b29bb36442115edfcad73f
Peter Smith <mail@petersmith.org>
2021-12-17 08:45:12 +1300

    Fix broken tags and move to folksonomy

--- Commit.sh
+++ Commit.sh
@@ -6,6 +6,9 @@ echo "The commit message is:    " \"${DESCRIPTION}\"
 echo "The working directory is :" `pwd`
 
 git add . &&  \
+sleep 2 && \
 git add -u && \
+sleep 2 && \
 git commit -m "${DESCRIPTION}" && \
+sleep 2 && \
 git push origin master
