af76a1515ddd25b7372546ff9fc7f8d381ada66f
Peter Smith <mail@petersmith.org>
2022-01-14 11:31:06 +1300

    Refactor webmentions

--- getWebmentions.sh
+++ getWebmentions.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-
-FILENAME="mentions.json"
-rm -fr ${FILENAME}
+FILENAME="responses.json"
+find . -name ${FILENAME} -delete
 
 webmention.io-backup -t ${WEBMENTION_KEY=} -d petersmith.org -cd content -f ${FILENAME}  -jf2 -tlo=false -p 
 
 #
 # Now fix the pesy hypens in the poperty names
 #
-find . -name mentions.json -exec sed -i  '
+find . -name ${FILENAME} -exec sed -i  '
 s/\(^ *\)"like-of":/\1"like_of":/
 s/\(^ *\)"mention-of":/\1"mention_of":/
 s/\(^ *\)"in-reply-to":/\1"in_reply_to":/
